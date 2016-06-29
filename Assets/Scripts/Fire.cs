2f20c4a0082c340ce9db6ecc0803f4b8d812eea5
Mauro Vanetti <posta@maurovanetti.info>
2016-06-29 03:02:37 +0200

    Fuoco figo

--- Assets/Scripts/Fire.cs
+++ Assets/Scripts/Fire.cs
@@ -8,26 +8,54 @@ public class Fire : MonoBehaviour
     public int framesPerSwitch;
 
     private SpriteRenderer sr;
+    private Transform flame;
+    private float flameSize;
+    private GameObject smoke;
 
     // Use this for initialization
     void Start()
     {
         sr = GetComponent<SpriteRenderer>();
+        flame = transform.Find("Flame");
+        if (flame != null)
+        {
+            flameSize = flame.localScale.x;
+            smoke = flame.Find("Smoke").gameObject;
+            if (!on)
+            {
+                flame.localScale = Vector3.zero;
+            }
+        }
     }
 
     // Update is called once per frame
     void Update()
     {
-        Color color = sr.color;
-        if (on && color.a < 1f)
-        {            
-            color.a = Mathf.Clamp(color.a + (1.0f / framesPerSwitch), 0f, 1f);
-            sr.color = color;            
-        }
-        else if (!on && color.a > 0f)
+        if (sr != null)
         {
-            color.a = Mathf.Clamp(color.a - (1.0f / framesPerSwitch), 0f, 1f);
-            sr.color = color;
+            Color color = sr.color;
+            if (on && color.a < 1f)
+            {
+                color.a = Mathf.Clamp(color.a + (1.0f / framesPerSwitch), 0f, 1f);
+                sr.color = color;
+            }
+            else if (!on && color.a > 0f)
+            {
+                color.a = Mathf.Clamp(color.a - (1.0f / framesPerSwitch), 0f, 1f);
+                sr.color = color;
+            }
+        }
+        if (flame != null)
+        {
+            if (on && flame.localScale.x < flameSize)
+            {
+                flame.localScale = Vector3.one * Mathf.Clamp(flame.localScale.x + (flameSize / framesPerSwitch), 0f, flameSize);
+            }
+            else if (!on && flame.localScale.x > 0f)
+            {
+                flame.localScale = Vector3.one * Mathf.Clamp(flame.localScale.x - (flameSize / framesPerSwitch), 0f, flameSize);
+            }
+            smoke.SetActive(on);
         }
     }
 }
