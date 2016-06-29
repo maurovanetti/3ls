76c6ebc82b4881289140ee5abc3f7b9cb6842d7d
Mauro Vanetti <posta@maurovanetti.info>
2016-06-29 21:59:03 +0200

    Aggiustata barba del verde + introdotto timeout

---
+++ Assets/Scripts/TimeBomb.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class TimeBomb : MonoBehaviour {
+
+    public float timeOut;
+    private float countDown;
+    private Vector3 lastMousePosition;
+
+    // Use this for initialization
+    void Start () {
+        countDown = timeOut;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        countDown -= Time.deltaTime;
+        if (Input.mousePosition != lastMousePosition)
+        {
+            countDown = timeOut;
+            lastMousePosition = Input.mousePosition;
+        }
+        if (countDown < 0f)
+        {
+            Debug.LogError("Timeout: " + timeOut + "\" of mouse inactivity. Quitting the game.");
+            Application.Quit();
+        }
+	}
+}
